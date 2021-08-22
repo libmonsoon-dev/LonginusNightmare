d0b5bbbcf419594b084072ae9c00f8fa01191e96
daniil_stepanenko <daniil.stepanenko.96@gmail.com>
2021-08-22 14:14:18 +0300

    fix iface

--- server/static.go
+++ server/static.go
@@ -1,13 +1,14 @@
 package server
 
 import (
+	"io/fs"
 	"net/http"
 
 	"github.com/valyala/fasthttp"
 	"github.com/valyala/fasthttp/fasthttpadaptor"
 )
 
-func NewStaticHandler(fs FS) fasthttp.RequestHandler {
+func NewStaticHandler(fs fs.FS) fasthttp.RequestHandler {
 	// TODO: migrate to fasthttp.FS
 	return fasthttpadaptor.NewFastHTTPHandler(http.FileServer(http.FS(fs)))
 }
